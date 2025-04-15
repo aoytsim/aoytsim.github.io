45ccf16b3189ae2302fc95fed2a25b72cb87cda8
aoytsim <161202719+aoytsim@users.noreply.github.com>
2025-04-15 10:24:18 +0800

    elements update

---
+++ js/header.js
@@ -0,0 +1,19 @@
+const template = document.createElement('template')
+template.innerHTML=`
+    <head>  
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>AOYTSIM</title>
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Fontaweome icons replacment -->
+        <link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css">
+        <!-- Theme styles for this template -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- Your Custom styles for this template -->
+        <link href="css/custom.css" rel="stylesheet">
+    </head>
+`
+
+document.body.appendChild(template.content)
